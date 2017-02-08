4d24d0df6635596f0f99a288ffa4703fcef9bafc
fuyuxian <fuyuxian@gomeholdings.com>
2017-02-08 15:19:57 +0800

    rename

---
+++ sample/src/main/java/com/github/sky/keyboard/MainActivity.java
@@ -0,0 +1,19 @@
+package com.github.sky.keyboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.github.sky.securitykeyboard.SecurityKeyboardManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.edit);
+        SecurityKeyboardManager.getInstance().initKeyboard(this, editText);
+    }
+}
